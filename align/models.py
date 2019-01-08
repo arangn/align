b5bdc52bff12aedf74b1f37618e2e5f6641d40a6
Naheed Arang <arang.na@gmail.com>
2019-01-08 11:17:42 -0800

    initial commit

---
+++ align/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Pose(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    image = models.ImageField(upload_to="align/photos/", null=True, blank=True)
+    description = models.CharField(max_length=500, null=False)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    password = models.CharField(max_length=255, null=False)
+    def __str__(self):
+        return self.name
+
+class SavedRoutine(models.Model):
+    INTENSITY_CHOICES = (
+    ('beginner', 'Beginner'),
+    ('intermediate', 'Intermediate'),
+    ('advanced', 'Advanced')
+    )
+
+    PRACTICE_CHOICES = (
+    ('strength', 'Strength'),
+    ('mobility', 'Mobility'),
+    ('balance', 'Balance'),
+    ('recovery', 'Recovery')
+    )
+
+    name = models.CharField(max_length=25)
+    user = models.ForeignKey(User, related_name='saved_routines', on_delete=models.CASCADE)
+    intensity = models.CharField(max_length=12, choices=INTENSITY_CHOICES)
+    practice = models.CharField(max_length=15, choices=PRACTICE_CHOICES)
+    targets = ArrayField(models.CharField(max_length=200), blank=True)
+    poses = models.ManyToManyField('align.Pose', related_name='routine_poses')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
