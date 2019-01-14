835386c84bb7ddd66b52d8c884d2551e2c1a1b0a
Naheed Arang <arang.na@gmail.com>
2019-01-14 13:31:12 -0800

    setup changes

--- api/settings.py
+++ api/settings.py
@@ -16,7 +16,7 @@ import os
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', "default_value")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
